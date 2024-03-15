c8d5aa387c7f92b7bb04d16454fc65c09886b7e5
rhoyer <rhoyerx@gmail.com>
2024-03-15 04:04:14 -0400

    ssd

--- Form_Register/index.php
+++ Form_Register/index.php
@@ -4,34 +4,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="css/bos">
+   <link rel="stylesheet" href="../css/bootstrap.min.css">
+   <link rel="stylesheet" href="../css/estilos.css">
     <title>Formulario de Registro de Alumnos</title>
 </head>
 <body>
+
     <h1>Registro de Alumnos</h1>
-    <form method="post">
-        <label for="id">ID:</label>
-        <input type="text" id="id" name="id" required><br>
+    <form method="post" class="form-horizontal">
+  <div class="form-group">
+    <label for="id" class="col-sm-2 control-label">ID:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="id" name="id" required>
+    </div>
+  </div>
 
-        <label for="cedula">Cédula:</label>
-        <input type="text" id="cedula" name="cedula" required><br>
+  <div class="form-group">
+    <label for="cedula" class="col-sm-2 control-label">Cédula:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="cedula" name="cedula" required>
+    </div>
+  </div>
 
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" required><br>
+  <div class="form-group">
+    <label for="nombre" class="col-sm-2 control-label">Nombre:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="nombre" name="nombre" required>
+    </div>
+  </div>
 
-        <label for="apellido">Apellido:</label>
-        <input type="text" id="apellido" name="apellido" required><br>
+  <div class="form-group">
+    <label for="apellido" class="col-sm-2 control-label">Apellido:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="apellido" name="apellido" required>
+    </div>
+  </div>
 
-        <label for="telefono">Teléfono:</label>
-        <input type="tel" id="telefono" name="telefono" required><br>
+  <div class="form-group">
+    <label for="telefono" class="col-sm-2 control-label">Teléfono:</label>
+    <div class="col-sm-10">
+      <input type="tel" class="form-control" id="telefono" name="telefono" required>
+    </div>
+  </div>
 
-        <label for="correo">Correo:</label>
-        <input type="email" id="correo" name="correo" required><br>
-
-        <input type="submit" name="registro" value="Guardar">
-    </form>
+  <div class="form-group">
+    <label for="correo" class="col-sm-2 control-label">Correo:</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="correo" name="correo" required>
+    </div>
+  </div>
+<br/>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <input type="submit" name="registro" value="Guardar" class="btn btn-primary">
+    </div>
+  </div>
+</form>
     <?php
     include("../logica/guardar_registro.php");  
     ?>
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
