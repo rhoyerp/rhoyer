69b9114f55dcd2ff6bc40f21bf273ef906a8d716
rhoyer <rhoyerx@gmail.com>
2024-03-14 23:58:58 -0400

    sdsds

--- Form_Register/index.php
+++ Form_Register/index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="../estilos.css">
     <title>Formulario de Registro de Alumnos</title>
 </head>
 <body>
